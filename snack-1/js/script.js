860dccfca911527d13710eb2c5ade94bdbf1ff1b
Emanuele Dinaro <ema.dinaro@gmail.com>
2024-12-12 16:40:34 +0100

    feat: add objects array (snack-1)

---
+++ snack-1/js/script.js
@@ -0,0 +1,23 @@
+// Snack 1
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore.
+
+const racingBikes = [
+    {
+        brandBike: "Lobito",
+        modelBike: "RV10",
+        weightBike: 8.9,
+    },
+    {
+        brandBike: "Van Rysel",
+        modelBike: "RCR PRO",
+        weightBike: 6.75,
+    },
+    {
+        brandBike: "Cannondale",
+        modelBike: "Synapse",
+        weightBike: 8,
+    }
+]
+
