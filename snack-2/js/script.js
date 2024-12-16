02ba8a25f3df32f7649284a0a12d8341dd4de872
Emanuele Dinaro <ema.dinaro@gmail.com>
2024-12-16 17:52:27 +0100

    feat: add js-snack2

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -5,30 +5,53 @@
 // Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 // Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 
-
-const teams = [
-    { 
-        name: "Juventus", 
-        points: 0,
-        fouls: 0,
+// 1. creare un array di oggetti di squadre di calcio avendo come chiavi: nome, punti fatti, e falli subiti.
+const footballTeams = [
+    {
+        teamName: 'Juventus',
+        point: 0,
+        fouls: 0
     },
-    { 
-        name: "Manchester City", 
-        points: 0,
-        fouls: 0,
+    {
+        teamName: 'Milan',
+        point: 0,
+        fouls: 0
     },
-    { 
-        name: "Borussia Dortmund", 
-        points: 0,
-        fouls: 0,
+    {
+        teamName: 'Inter',
+        point: 0,
+        fouls: 0
     },
-    { 
-        name: "Athletico Madrid", 
-        points: 0,
-        fouls: 0,
-    }
+    {
+        teamName: 'Roma',
+        point: 0,
+        fouls: 0
+    },
 ]
-console.log(teams);
+console.log(footballTeams)
 
-//per dopo: usare per i punti: Math.floor(Math.random() * 90),
-//per i falli Math.floor(Math.random() * 20)
+// 2. tramite la function getRndInteger, assegnare a points.value e fouls.value un numero casuale.
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+// footballTeams[0].point = getRndInteger(0, 102);
+for (let i = 0; i < footballTeams.length; i++) {
+    const team = footballTeams[i]
+    team.point = getRndInteger(0, 102)
+    team.fouls = getRndInteger(0, 50)
+}
+
+console.log(footballTeams)
+
+// 3. creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const newFootballTeam = [];
+for (let i = 0; i < footballTeams.length; i++) {
+    const team = footballTeams[i];
+    const obj = {
+        teamName: team.teamName,
+        fouls: team.fouls
+    }
+    newFootballTeam.push(obj)
+}
+console.log(newFootballTeam)
